ee57ac691fd58e4c099499b22fb058041a92f030
CekanLucas <cekanlucas@gmail.com>
2020-12-16 23:11:31 -0500

    refactored for typescript

---
+++ pages/api/hello.ts
@@ -0,0 +1,6 @@
+import { NextApiRequest, NextApiResponse } from 'next'
+
+// export default function handler(req, res) {
+export default (_: NextApiRequest, res: NextApiResponse) => {
+  res.status(200).json({ text: 'Hello' })
+}
